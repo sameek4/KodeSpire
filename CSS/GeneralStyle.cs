08aff8f3381948ca99ee5418b0c626762519819a
sameek4 <sameek4@gmail.com>
2019-03-01 12:56:34 +0530

    Add files via upload

---
+++ CSS/GeneralStyle.cs
@@ -0,0 +1,30 @@
+.TutorialsHeader
+   {
+
+      background: url(../Images/Tutorials/Background.png);
+      width: 100%;
+      height:auto;
+
+   }
+
+  .TutorialsHeader > .Title
+  {
+    font-size: 3rem;
+    font-family:   Trebuchet MS;
+    color: white;
+    text-shadow: 2px 2px 4px #000000;
+  	text-align: center;
+  	justify-content: center;
+    font-weight: bold;
+  }
+
+  .TutorialsHeader > .Subtitle
+  {
+    font-size: 1.5rem;
+    font-family: Palatino;
+    color:#dfedf1;
+    text-align: center;
+    justify-content: center;
+    font-weight: bold;
+   text-shadow: 1px 1px 2px #000000;
+  }
